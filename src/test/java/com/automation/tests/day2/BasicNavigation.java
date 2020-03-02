36ddace70d7858ca501b49df1e7b87a87dc2dcef
AyseYB <yekelerbilgic@gmail.com>
2020-03-01 22:05:30 -0600

    Created basic navigation class

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -6,13 +6,29 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigation {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         //to start selenium script we need:
         //setUp webdriver (browser driver ) and create webdriver object
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         //in selenium everything starts from WebDriver Interface
+        //ChromeDriver extends RemoteWebDrivet --->webdriver object
         driver.get("http://google.com"); // to open a website
+        Thread.sleep(3000);
+        //method that return page title
+// you can also see it as tab name, in the browser
+        String title = driver.getTitle(); // returns <title> some title</title> text
+        String expectedTitle = "Google";
+        System.out.println("title is ... " + title);
+        if(expectedTitle.equals(title)){
+            System.out.println("TEST PASSED");
+        }else {
+            System.out.println("TEST FAILED");
+        }
+
+
+        //MUST BE AT THE END
+        driver.close();// to close browser
 
     }
 }
